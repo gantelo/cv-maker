335b692bf99d64d5d498c0a40311b8f28c53fe9a
ms-gantelo <gantelo@makingsense.com>
2020-12-24 14:22:59 -0300

    :gear: Add eslint + prettier

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'comma-dangle': 0,
+    'react-hooks/exhaustive-deps': 0,
+  },
+};
